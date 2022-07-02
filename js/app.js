2468a8ec7fc8df192691c97a4b524eb37c728640
Indra Tanaya <aryaindratanaya@gmail.com>
2022-07-03 01:28:30 +0800

    Add a navbar

--- js/app.js
+++ js/app.js
@@ -1,9 +1,1 @@
 const app = angular.module("app", []);
-
-app.controller("HomeCtrl", [
-  "$scope",
-  function ($scope) {
-    $scope.angularMessage = "Hi, the AngularJS is working!";
-    $("#j-query-message").text("Yep, the jQuery is also working!");
-  },
-]);
