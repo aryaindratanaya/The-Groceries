6f634d7fc39aad4603bd2ae454a91a9eb05a0674
Indra Tanaya <aryaindratanaya@gmail.com>
2022-07-16 21:58:53 +0800

    Order the grocery list by date

--- features/grocery-list/groceryList.component.js
+++ features/grocery-list/groceryList.component.js
@@ -5,14 +5,14 @@ angular
     templateUrl: "/features/grocery-list/groceryList.template.html",
     controller: function () {
       this.groceries = [
-        { completed: true, name: "milk", date: "2014-10-1" },
-        { completed: true, name: "cookies", date: "2014-10-1" },
-        { completed: true, name: "ice cream", date: "2014-10-1" },
-        { completed: true, name: "potatoes", date: "2014-10-1" },
-        { completed: true, name: "cereal", date: "2014-10-1" },
-        { completed: true, name: "bread", date: "2014-10-1" },
-        { completed: true, name: "eggs", date: "2014-10-1" },
-        { completed: true, name: "tortillas", date: "2014-10-1" },
+        { completed: true, name: "milk", date: "2014-10-01" },
+        { completed: true, name: "cookies", date: "2014-10-02" },
+        { completed: true, name: "ice cream", date: "2014-10-03" },
+        { completed: true, name: "potatoes", date: "2014-10-04" },
+        { completed: true, name: "cereal", date: "2014-10-05" },
+        { completed: true, name: "bread", date: "2014-10-06" },
+        { completed: true, name: "eggs", date: "2014-10-07" },
+        { completed: true, name: "tortillas", date: "2014-10-08" },
       ];
     },
   });
